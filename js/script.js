eea21e05ac30b13ea5e01d831268d9bb6be8f190
default <yunus.emre.guzel@mths.ca>
2025-05-05 14:04:47 +0000

    finshed the program

--- js/script.js
+++ js/script.js
@@ -6,27 +6,48 @@
 // This file contains the JS functions for index.html
 
 "use strict"
-// Setting the functoin 
+
 function numberLoopCheck() {
-  // Setting the variables
-  const firstNum = parseInt(document.getElementById("first-num").value)
-  const secondNum = parseInt(document.getElementById("second-num").value)
+  // Get numbers from the user
+  let firstNum = parseInt(document.getElementById("first-num").value)
+  let secondNum = parseInt(document.getElementById("second-num").value)
+
   let result = 0
   let count = 0
   let additionText = ""
+  let negativeResult = false
 
-  // setting the while loop 
-  // Repeats the code inside the loop until count reaches secondNum
+  // Check if the result should be negative
+  if ((firstNum < 0 && secondNum > 0) || (firstNum > 0 && secondNum < 0)) {
+    negativeResult = true
+  }
+
+  // Make both numbers positive
+  if (firstNum < 0) {
+    firstNum = 0 - firstNum
+  }
+
+  if (secondNum < 0) {
+    secondNum = 0 - secondNum
+  }
+
+  // Multiply using repeated addition
   while (count < secondNum) {
     result += firstNum
     additionText += firstNum
-    //If secondNum is 3, we only want to add + when count is 0 
+
     if (count < secondNum - 1) {
       additionText += " + "
     }
 
     count++
   }
-  // Getting the result
+
+  // Make result negative if needed
+  if (negativeResult) {
+    result = 0 - result
+  }
+
+  // Show result
   document.getElementById("result").textContent = additionText + " = " + result
 }
